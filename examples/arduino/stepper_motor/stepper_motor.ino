ad196e060f138dd2f7917c7a080906526783b79c
Abdul Salam A Haris <salam@cytron.io>
2024-10-15 09:15:53 +0800

    Edited stepper motor code

--- examples/arduino/stepper_motor/stepper_motor.ino
+++ examples/arduino/stepper_motor/stepper_motor.ino
@@ -11,13 +11,11 @@ EMAIL    : support@cytron.io
 const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution for your motor
 
 // initialize the stepper library on DC Motor Driver pins:
-Stepper myStepper1(stepsPerRevolution, 9, 8, 11, 10); 
-Stepper myStepper2(stepsPerRevolution, 12, 13, 14, 15);
+Stepper myStepper(stepsPerRevolution, 9, 8, 11, 10); 
 
 void setup() {
   // set the speed at 60 rpm:
-  myStepper1.setSpeed(60);
-  myStepper2.setSpeed(60);
+  myStepper.setSpeed(60);
   // initialize the serial port:
   Serial.begin(9600);
 }
@@ -25,13 +23,11 @@ void setup() {
 void loop() {
   // step one revolution  in one direction:
   Serial.println("clockwise");
-  myStepper1.step(stepsPerRevolution);
-  myStepper2.step(stepsPerRevolution);
+  myStepper.step(-stepsPerRevolution);
   delay(500);
 
   // step one revolution in the other direction:
   Serial.println("counterclockwise");
-  myStepper1.step(-stepsPerRevolution);
-  myStepper2.step(-stepsPerRevolution);
+  myStepper.step(stepsPerRevolution);
   delay(500);
 }
